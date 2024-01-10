b063ad8fcb2615964418249d84da4c855bc1f5e3
Tueik <ricardoespartan2013@gmail.com>
2024-01-10 14:58:16 -0600

    Configuración de método doGet para carga de datos usuarios

--- TC_ClinicaOdontologica/src/main/java/com/clinicaodontologica/mx/servlets/SvUsuarios.java
+++ TC_ClinicaOdontologica/src/main/java/com/clinicaodontologica/mx/servlets/SvUsuarios.java
@@ -5,12 +5,16 @@
 package com.clinicaodontologica.mx.servlets;
 
 import com.clinicaodontologica.mx.logica.ControladoraLogica;
+import com.clinicaodontologica.mx.logica.Usuario;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -30,7 +34,15 @@ public class SvUsuarios extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+        listaUsuarios = control.obtenerUsuarios();
+        
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("listaUsuarios", listaUsuarios);
+        
+        response.sendRedirect("verUsuarios.jsp");
+        
     }
 
     
