25766f87918878e23851933ab4ab1d93b52552f5
Tueik <ricardoespartan2013@gmail.com>
2024-01-11 13:01:03 -0600

    Creación del servlet para eliminar usuarios

---
+++ TC_ClinicaOdontologica/src/main/java/com/clinicaodontologica/mx/servlets/SvEliminarUsuarios.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.clinicaodontologica.mx.servlets;
+
+import com.clinicaodontologica.mx.logica.ControladoraLogica;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ricar
+ */
+@WebServlet(name = "SvEliminarUsuarios", urlPatterns = {"/SvEliminarUsuarios"})
+public class SvEliminarUsuarios extends HttpServlet {
+
+    ControladoraLogica control = new ControladoraLogica();
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        
+        control.eliminarUsuario(id);
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
